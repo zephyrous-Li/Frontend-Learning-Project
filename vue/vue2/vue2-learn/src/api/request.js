dd7395a05f8a054ea75969a4d6979beffad22e9a
zephyr_Li <695832969@qq.com>
2024-11-08 20:38:28 +0800

    1

--- vue/vue2/vue2-learn/src/api/request.js
+++ vue/vue2/vue2-learn/src/api/request.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({});
 // 相应拦截器
 request.interceptors.response.use(function (resp) {
-  console.log(resp, "运行了拦截器1");
+  console.log(resp, "运行了拦截器");
   if (resp.code != 0) {
     // 错误吹
     return null;
